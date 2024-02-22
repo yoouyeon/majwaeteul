1a09a20e0d4df2b758b5514cfe170a6f7da97591
yoouyeon <jyeon.yoon59@gmail.com>
2024-02-22 14:46:23 +0900

    [2️⃣ LEVEL 2] 이진 변환 반복하기
    다른 풀이 추가

--- Programmers/이진_변환_반복하기.js
+++ Programmers/이진_변환_반복하기.js
@@ -1,4 +1,4 @@
-function solution(s) {
+function solution1(s) {
   let zero = 0; // 총 제거되는 0의 개수
   let count = 0; // 총 이진변환의 횟수
   while (s !== "1") {
@@ -12,6 +12,20 @@ function solution(s) {
     const c = prevC - removedZero; // 0이 제거된 s의 길이
     zero += removedZero;
     s = c.toString(2); // 2진법으로 표현한 문자열
+    count++;
+  }
+  return [count, zero];
+}
+
+function solution2(s) {
+  let zero = 0;
+  let count = 0;
+  while (s !== "1") {
+    const prevLength = s.length;
+    s = s.replaceAll("0", "");
+    const currLength = s.length;
+    zero += prevLength - currLength;
+    s = s.length.toString(2);
     count++;
   }
   return [count, zero];
