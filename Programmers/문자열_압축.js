e2c3a916332203c1251b2f87886f0b7597c87ef8
yoouyeon <jyeon.yoon59@gmail.com>
2024-06-03 20:03:31 +0900

    [2️⃣ LEVEL 2] 문자열 압축

---
+++ Programmers/문자열_압축.js
@@ -0,0 +1,41 @@
+function solution(s) {
+  let answer = 1000;
+  // step 0. 문자열의 길이가 1인 경우 더 이상 압축할 수 없으므로 1을 반환
+  if (s.length === 1) return 1;
+  for (let subLength = 1; subLength < s.length; subLength++) {
+    // step 1. 분할
+    let startIdx = 0;
+    const subStrArr = [];
+    while (s.slice(startIdx, startIdx + subLength) !== "") {
+      subStrArr.push(s.slice(startIdx, startIdx + subLength));
+      startIdx += subLength;
+    }
+    // console.log(subStrArr);
+    // step 2. 압축
+    let currIdx = 0;
+    let compressedStr = "";
+    while (currIdx < subStrArr.length) {
+      if (currIdx === subStrArr.length - 1) {
+        compressedStr += subStrArr[currIdx];
+        break;
+      }
+      let count = 1;
+      while (
+        currIdx < subStrArr.length - 1 &&
+        subStrArr[currIdx] === subStrArr[currIdx + 1]
+      ) {
+        currIdx++;
+        count++;
+      }
+      if (count == 1) {
+        compressedStr += subStrArr[currIdx];
+      } else {
+        compressedStr += subStrArr[currIdx] + count.toString();
+      }
+      currIdx++;
+    }
+    // console.log(compressedStr);
+    if (answer > compressedStr.length) answer = compressedStr.length;
+  }
+  return answer;
+}
