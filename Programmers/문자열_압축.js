bb2ae0a300f345af286969a49f8b91016da6282c
yoouyeon <jyeon.yoon59@gmail.com>
2024-06-04 14:37:51 +0900

    [2️⃣ LEVEL 2] 문자열 압축 압축하는 길이를 문자열의 절반까지만 확인

--- Programmers/문자열_압축.js
+++ Programmers/문자열_압축.js
@@ -2,7 +2,7 @@ function solution(s) {
   let answer = 1000;
   // step 0. 문자열의 길이가 1인 경우 더 이상 압축할 수 없으므로 1을 반환
   if (s.length === 1) return 1;
-  for (let subLength = 1; subLength < s.length; subLength++) {
+  for (let subLength = 1; subLength <= Math.floor(s.length / 2); subLength++) {
     // step 1. 분할
     let startIdx = 0;
     const subStrArr = [];
